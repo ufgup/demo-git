258217bc0dc5d3ce6630337129becad5e70051d6
Daniel Melo <danielmelogpi@gmail.com>
2014-11-24 23:45:23 -0200

    Adicionado mecanismo de sorteio e um loop legal

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Ola, Git!");
+		System.out.println("Resultado do sorteio:" + meuInteiro());
+		meuLoop();
+	}
+	
+	public static int meuInteiro() {
+		return (int) Math.floor(Math.random()*100);
+	}
+	
+	public static void meuLoop() {
+		
+		int i = 30;
+		while (i > 0){
+			System.out.print(" [ " + i + " ] ");
+			if (i%14 == 0) {
+				System.out.println();
+			}
+			i--;
+		}
+		
 	}
 	
 }
